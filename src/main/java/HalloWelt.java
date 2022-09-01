d4521b3e8d5328a62cf91c720f6ac3d320da7f9a
sebastianwitt <witt.seb@outlook.de>
2022-09-01 17:11:34 +0200

    HalloWelt

---
+++ src/main/java/HalloWelt.java
@@ -0,0 +1,10 @@
+public class HalloWelt {
+    public static void main(String[] args) {
+
+        String name = "Seb";
+
+        System.out.println("Hallo Welt!");
+        System.out.println("Hier beginnt alles.");
+        System.out.println("Mein Name ist " + name + ".");
+    }
+}
